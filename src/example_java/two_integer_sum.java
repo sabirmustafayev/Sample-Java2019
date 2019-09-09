e5324e47c5a49f8e72461592597894cb182966de
sabir <sabirmustafayev86@gmail.com>
2019-09-09 16:21:10 -0400

    first

---
+++ src/example_java/two_integer_sum.java
@@ -0,0 +1,13 @@
+package example_java;
+
+public class two_integer_sum {
+	public static void main(String[] args) {
+		
+		int a = 10, b = 15;
+		
+		int sum = a + b;
+		
+		System.out.println(sum);
+	}
+
+}
